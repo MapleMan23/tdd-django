d3ccad6649e3484d6a34e688163eb2b0ea9aada8
Hayden Rode <hayden.rode@stresearch.com>
2021-06-15 14:39:30 -0400

    Refactor functional test to get rid of duplication

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ def browser():
 
     firefox.quit()
 
+def check_for_row_in_list_tabe(browser: webdriver.Firefox, row_text: str):
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    assert row_text in [row.text for row in rows]
+
 @pytest.mark.functional_test
 def test_can_start_a_list_and_retrieve_it_later(browser: webdriver.Firefox):
     # Edith has heard about a cool new online to-do app. She goes
@@ -35,10 +40,7 @@ def test_can_start_a_list_and_retrieve_it_later(browser: webdriver.Firefox):
     # "1: Buy peacock feathers" as an item in a to-do list table
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
-
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    assert '1: Buy peacock feathers' in [row.text for row in rows]
+    check_for_row_in_list_tabe(browser, '1: Buy peacock feathers')
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very
@@ -49,10 +51,8 @@ def test_can_start_a_list_and_retrieve_it_later(browser: webdriver.Firefox):
     time.sleep(1)
 
     # The page updates again, and now shows both items on her list
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    assert '1: Buy peacock feathers' in [row.text for row in rows]
-    assert '2: Use peacock feathers to make a fly' in [row.text for row in rows]
+    check_for_row_in_list_tabe(browser, '1: Buy peacock feathers')
+    check_for_row_in_list_tabe(browser, '2: Use peacock feathers to make a fly')
 
     # Edith wonders whether the site will remember her list. Then she sees
     # that the site has generated a unique URL for her -- there is some
