f8decd0136fbb87b5e0207ee42f4934a4949e0f6
Hayden Rode <hayden.rode@stresearch.com>
2021-06-15 12:45:32 -0400

    Refactor to use templates and django test client

--- lists/tests.py
+++ lists/tests.py
@@ -1,18 +1,12 @@
+import django
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 
+from pytest_django.asserts import assertTemplateUsed
+
 
 # Create your tests here.
-def test_root_url_resolves_to_home_page_view():
-    found = resolve('/')
-    assert found.func == home_page
-
-def test_home_page_returns_correct_html():
-    request = HttpRequest()
-    response = home_page(request)
-    html = response.content.decode('utf8')
-
-    assert html.startswith('<html>')
-    assert '<title>To-Do lists</title>' in html
-    assert html.endswith('</html>')
+def test_home_page_returns_correct_html(client: django.test.Client):
+    response = client.get('/')
+    assertTemplateUsed(response, 'home.html')
